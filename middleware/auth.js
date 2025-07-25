d393c36903b15895ce810e976e33743636d40502
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-07-25 23:58:10 +0530

    fixed minor issue in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ exports.protect = async (req, res, next) => {
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startswith("Bearer")
+    req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
