5d17568504ee34c1c069f6f59eb69117e662a6b6
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-07-25 20:30:06 +0530

    user routes defined

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { protect, authorize } = require("../middleware/auth");
+const {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteuser,
+} = require("../controllers/user");
+
+const router = express.Router();
+
+router.use(protect);
+router.use(authorize("librarian"));
+
+router.route("/").get(getUsers);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteuser);
