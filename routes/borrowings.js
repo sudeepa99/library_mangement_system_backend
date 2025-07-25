12cc038f046d43123f9765901a95498779fe3690
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-07-25 21:09:52 +0530

    borrowings route defined

---
+++ routes/borrowings.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { protect, authorize } = require("../middleware/auth");
+const {
+  getBorrowings,
+  createBorrowing,
+  getBorrowing,
+  getOverdueBorrowings,
+  returnBook,
+  getUserBorrowings,
+} = require("../controllers/borrowings");
+
+const router = express.Router();
+
+router.use(protect);
+
+router
+  .route("/")
+  .get(authorize("librarian"), getBorrowings)
+  .post(authorize("librarian"), createBorrowing);
+
+router.route("/overdue").get(authorize("librarian"), getOverdueBorrowings);
+
+router.route("/:id/return").put(authorize("librarian", returnBook));
+
+router.route("/:id").get(authorize("librarian"), getBorrowing);
+
+router.route("/user/:userId").get(getUserBorrowings);
+
+module.exports = router;
