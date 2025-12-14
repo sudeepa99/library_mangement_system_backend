3b7c8a67804424fe9d53ea36999226d8855f1d51
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-12-14 12:48:53 +0530

    server file updated with socket integration

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 require("dotenv").config();
 const app = require("./app");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const PORT = process.env.PORT;
 
-const server = app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const server = http.createServer(app);
 
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server & exit process
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.set("io", io);
+
+io.on("connection", (socket) => {
+  console.log("Admin Connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Admin Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
   server.close(() => process.exit(1));
 });
