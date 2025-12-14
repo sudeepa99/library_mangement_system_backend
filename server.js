c95bf8116725652ee6945ca36cb6281ac162c659
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-12-14 22:22:20 +0530

    server file reverted

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 require("dotenv").config();
 const app = require("./app");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const PORT = process.env.PORT;
 
-const server = http.createServer(app);
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-app.set("io", io);
-
-io.on("connection", (socket) => {
-  console.log("Admin Connected", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("Admin Disconnected", socket.id);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
   server.close(() => process.exit(1));
 });
