ad440fe90383cb4ed4312fabafb67dbeb2359bc4
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-20 20:05:13 +0530

    defined route for get recent activity function

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { protect, authorize } = require("../middleware/auth");
-const { getAdminStats } = require("../controllers/dashboardStats");
+const {
+  getAdminStats,
+  getRecentActivity,
+} = require("../controllers/dashboardStats");
 
 const router = express.Router();
 
 router.use(protect);
 router.use(authorize("librarian"));
 router.route("/admin-stats").get(getAdminStats);
+router.route("/recent-activity").get(getRecentActivity);
 
 module.exports = router;
