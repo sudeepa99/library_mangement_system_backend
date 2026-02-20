317ceba9f728334cf58067e4ae7cd55be0f40846
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-20 11:50:31 +0530

    added route for update me api

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   sendResetCode,
   verifyresetCode,
   resetPasswordWithCode,
+  updateMe,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -19,5 +20,6 @@ router.get("/me", protect, getMe);
 router.post("/forgot-password/send-code", sendResetCode);
 router.post("/forgot-password/verify-code", verifyresetCode);
 router.post("/forgot-password/reset", resetPasswordWithCode);
+router.put("/update-me", protect, updateMe);
 
 module.exports = router;
