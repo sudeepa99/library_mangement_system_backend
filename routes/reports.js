66fb71e88a06e2be25b5463ee480cbf04966c74f
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-21 13:01:27 +0530

    defined route for get key metrics api

--- routes/reports.js
+++ routes/reports.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
-const { getBorrowingTrends } = require("../controllers/reports");
+const { getBorrowingTrends, getKeyMetrics } = require("../controllers/reports");
 
 router.use(protect);
 router.use(authorize("librarian"));
 router.get("/borrowing-trends", getBorrowingTrends);
+router.get("/key-metrics", getKeyMetrics);
 
 module.exports = router;
