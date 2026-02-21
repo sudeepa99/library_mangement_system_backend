431735c41c5e3da8da02a9419d85f3c1e5b7d7d4
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-21 22:39:37 +0530

    defined route for getAnalyticsReport api

--- routes/reports.js
+++ routes/reports.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
-const { getBorrowingTrends, getKeyMetrics } = require("../controllers/reports");
+const {
+  getBorrowingTrends,
+  getKeyMetrics,
+  getAnalyticsReport,
+} = require("../controllers/reports");
 
 router.use(protect);
 router.use(authorize("librarian"));
 router.get("/borrowing-trends", getBorrowingTrends);
 router.get("/key-metrics", getKeyMetrics);
+router.get("/analytics", getAnalyticsReport);
 
 module.exports = router;
