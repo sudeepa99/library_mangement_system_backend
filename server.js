8b5bfce500f0b57e8bb66700b339982817dec46a
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-22 12:50:59 +0530

    removed unwanted consoles

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT;
 
-const server = app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
-);
+const server = app.listen(PORT);
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
   server.close(() => process.exit(1));
 });
